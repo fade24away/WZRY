a43d70af5e82bd8fefd8f4aad0174360e3a364d6
zhichang <952734220@qq.com>
2020-07-22 20:43:58 +0800

    完成了内容管理模块的开发

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
 import Main from '../views/Main.vue'
+import CategoryEdit from '../views/CategoryEdit.vue'
+import CategoryList from '../views/CategoryList.vue'
+import ItemEdit from '../views/ItemEdit.vue'
+import ItemList from '../views/ItemList.vue'
 
 Vue.use(VueRouter)
 
@@ -9,7 +12,15 @@ const routes = [
   {
     path: '/',
     name: 'Main',
-    component: Main
+    component: Main,
+    children: [
+      { path: '/categories/create', component: CategoryEdit },
+      { path: '/categories/edit/:id', component: CategoryEdit, props: true },
+      { path: '/categories/list', component: CategoryList },
+      { path: '/items/create', component: ItemEdit },
+      { path: '/items/edit/:id', component: ItemEdit, props: true },
+      { path: '/items/list', component: ItemList }
+    ]
   }
 ]
 
